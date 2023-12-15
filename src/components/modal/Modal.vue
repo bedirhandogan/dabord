24e16469cf0e1428525a348138abc3d6f09de367
bedirhandogan <bedirhandgn14@gmail.com>
2023-12-15 21:18:41 +0300

    refactor: always keep the modal in the middle

--- src/components/modal/Modal.vue
+++ src/components/modal/Modal.vue
@@ -27,9 +27,10 @@ export default {
 
 <style scoped>
 .modal-container {
-    position: absolute;
-    top: 0;
-    left: 0;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     background-color: var(--color-black-metal-95);
     width: 100%;
     height: 100vh;
