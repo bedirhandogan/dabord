e3f33a05dbdcf7573e4574309df97643617c73b2
bedirhandogan <bedirhandgn14@gmail.com>
2023-12-17 15:16:40 +0300

    feat: add tooltip component

---
+++ src/components/shared/Tooltip.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        value: {
+            type: String
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="tooltip">
+        <slot />
+        <div class="text">{{ this.value }}</div>
+    </div>
+</template>
+
+<style scoped>
+.tooltip {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    position: relative;
+}
+
+.tooltip:hover .text {
+    display: flex;
+}
+
+.text {
+    display: none;
+    position: absolute;
+    top: calc(100% + 5px);
+    width: max-content;
+    font-size: 12px;
+    padding: 5px 8px 6px 8px;
+    background-color: var(--color-existential-angst);
+    border: 1px solid var(--color-eerie-black);
+    border-radius: 5px;
+    user-select: none;
+}
+</style>
