0db852a5f2a57a9078b6138ee871d142fbd0ae5c
bedirhandogan <bedirhandgn14@gmail.com>
2023-12-19 13:56:58 +0300

    feat: add atan2 function tester component

---
+++ src/components/testers/functions/atan2.vue
@@ -0,0 +1,252 @@
+<script>
+import { defineComponent } from 'vue'
+import {
+    Button,
+    Buttons,
+    CodeBlock,
+    Controller,
+    Header,
+    Main,
+    Overview,
+    Title,
+    Container,
+    Information,
+    Property,
+    Indicator,
+    Area,
+    Converted,
+    Value
+} from '@/components/shared/TestComponentStructure'
+import Tooltip from '@/components/shared/Tooltip.vue'
+import { useLanguageStore } from '@/stores/language'
+import RangeSlider from '@/components/shared/RangeSlider.vue'
+import Modal from '@/components/modal/Modal.vue'
+import BrowserCompatibility from '@/components/modal/BrowserCompatibility.vue'
+import { copyCodeSnippetAndNotify, getReferenceURL, getSupportList } from '@/utils'
+
+const codeSnippet = (y, x) => `.octagon {
+    transform: rotate(atan2(${y}, ${x}));
+}`
+
+export default defineComponent({
+    components: {
+        BrowserCompatibility,
+        Modal,
+        Indicator,
+        RangeSlider,
+        Area,
+        Converted,
+        Value,
+        Property,
+        Information,
+        Tooltip,
+        CodeBlock,
+        Controller,
+        Overview,
+        Main,
+        Buttons,
+        Button,
+        Title,
+        Header,
+        Container
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'Untitled'
+        }
+    },
+    setup() {
+        const language = useLanguageStore()
+
+        return {
+            language
+        }
+    },
+    data() {
+        return {
+            showCodeBlock: false,
+            code: codeSnippet(0.5, -0.5),
+            isCopied: false,
+            propertyValueY: 0,
+            propertyValueX: 0,
+            showBrowserCompatibility: false
+        }
+    },
+    methods: {
+        copyCodeSnippetAndNotify,
+        getReferenceURL,
+        getSupportList,
+        changePropertyValue(state, variable) {
+            const integers = [
+                ...Array.from({ length: 10 }, (_, index) => (-index - 1) / 10).reverse(),
+                0,
+                ...Array.from({ length: 10 }, (_, index) => (index + 1) / 10)
+            ]
+
+            // Normalize the slider state value to fit within the range of the 'integers' array indices,
+            // ensuring it won't go beyond the bounds. The result is assigned to 'propertyValue'.
+            const index = Math.round((state / 100) * (integers.length - 1))
+
+            if (variable === 'y') this.propertyValueY = integers[index]
+            if (variable === 'x') this.propertyValueX = integers[index]
+
+            this.code = codeSnippet(this.propertyValueY, this.propertyValueX)
+        }
+    }
+})
+</script>
+
+<template>
+    <Container>
+        <Header>
+            <Title>{{ this.title }}</Title>
+            <Buttons title="">
+                <Tooltip :value="language.translate('tipCode')">
+                    <Button @click="this.showCodeBlock = true">
+                        <img src="@/assets/svg/code.svg" alt="code" />
+                    </Button>
+                </Tooltip>
+                <Tooltip
+                    :value="language.translate('tipBrowser')"
+                    @click="this.showBrowserCompatibility = true"
+                >
+                    <Button>
+                        <img src="@/assets/svg/information.svg" alt="support" />
+                    </Button>
+                </Tooltip>
+            </Buttons>
+        </Header>
+
+        <Main>
+            <Overview>
+                <div class="overview-child-wrapper">
+                    <div
+                        class="octagon"
+                        :style="{
+                            transform: `rotate(${Math.round(
+                                (Math.atan2(this.propertyValueY, this.propertyValueX) * 180) /
+                                    Math.PI
+                            )}deg)`
+                        }"
+                    />
+                </div>
+            </Overview>
+            <Controller>
+                <Information>
+                    <Title style="font-size: 16px"> Transform Property </Title>
+                    <Property>
+                        transform:
+                        <Value
+                            >rotate ( atan2 (
+                            <span style="color: whitesmoke"
+                                >{{ this.propertyValueY }}, {{ this.propertyValueX }}</span
+                            >
+                            ) )</Value
+                        >
+                    </Property>
+                    <Converted v-if="language.data.selected === 'en'">
+                        value converted from
+                        <Value style="padding: 5px; color: whitesmoke"
+                            ><span style="font-family: var(--font-family-source-code)">Y:</span>
+                            {{ this.propertyValueY }}, <span style="margin: 0 3px" />
+                            <span style="font-family: var(--font-family-source-code)">X:</span>
+                            {{ this.propertyValueX }}</Value
+                        >
+                        to
+                        <Value style="padding: 5px; color: whitesmoke" id="length-limit"
+                            >{{
+                                Math.round(
+                                    (Math.atan2(this.propertyValueY, this.propertyValueX) * 180) /
+                                        Math.PI
+                                )
+                            }}°</Value
+                        >
+                    </Converted>
+                    <Converted v-else-if="language.data.selected === 'tr'">
+                        değer
+                        <Value style="padding: 5px; color: whitesmoke"
+                            ><span style="font-family: var(--font-family-source-code)">Y:</span>
+                            {{ this.propertyValueY }}, <span style="margin: 0 3px" />
+                            <span style="font-family: var(--font-family-source-code)">X:</span>
+                            {{ this.propertyValueX }}</Value
+                        >
+                        'den
+                        <Value style="padding: 5px; color: whitesmoke" id="length-limit"
+                            >{{
+                                Math.round(
+                                    (Math.atan2(this.propertyValueY, this.propertyValueX) * 180) /
+                                        Math.PI
+                                )
+                            }}°</Value
+                        >'e dönüştürüldü
+                    </Converted>
+                </Information>
+
+                <Area style="gap: 15px">
+                    <div class="range-slider-wrapper">
+                        Y:
+                        <RangeSlider
+                            default-value="75"
+                            style="width: 100%"
+                            :value="(state) => changePropertyValue(state, 'y')"
+                        />
+                    </div>
+                    <div class="range-slider-wrapper">
+                        X:
+                        <RangeSlider
+                            default-value="25"
+                            style="width: 100%"
+                            :value="(state) => changePropertyValue(state, 'x')"
+                        />
+                    </div>
+                    <Indicator :value="['-', '0', '+']" style="margin-top: -5px" />
+                </Area>
+            </Controller>
+        </Main>
+
+        <CodeBlock :value="this.code" v-show="this.showCodeBlock">
+            <Tooltip
+                :value="
+                    this.isCopied ? language.translate('tipCopied') : language.translate('tipCopy')
+                "
+            >
+                <Button @click="copyCodeSnippetAndNotify(this)">
+                    <img src="@/assets/svg/copy.svg" alt="copy" />
+                </Button>
+            </Tooltip>
+            <Tooltip :value="language.translate('tipClose')">
+                <Button @click="this.showCodeBlock = false">
+                    <img src="@/assets/svg/close.svg" alt="close" />
+                </Button>
+            </Tooltip>
+        </CodeBlock>
+    </Container>
+
+    <Modal :state="this.showBrowserCompatibility">
+        <BrowserCompatibility
+            :state="(state) => (this.showBrowserCompatibility = state)"
+            :data="getSupportList('types', 'atan2')"
+            :reference="getReferenceURL('types', 'atan2')"
+        />
+    </Modal>
+</template>
+
+<style scoped>
+.overview-child-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.range-slider-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    font-size: 13px;
+    font-family: var(--font-family-source-code);
+    color: var(--color-cold-grey);
+}
+</style>
--- src/components/testers/index.js
+++ src/components/testers/index.js
@@ -2,13 +2,15 @@ import abs from '@/components/testers/functions/abs.vue'
 import acos from '@/components/testers/functions/acos.vue'
 import asin from '@/components/testers/functions/asin.vue'
 import atan from '@/components/testers/functions/atan.vue'
+import atan2 from '@/components/testers/functions/atan2.vue'
 
 const testers = {
     functions: {
         'abs()': abs,
         'acos()': acos,
         'asin()': asin,
-        'atan()': atan
+        'atan()': atan,
+        'atan2()': atan2
     }
 }
 
