f2587ff98d2cae36a68968a7b6e058d0437a7191
bedirhandogan <bedirhandgn14@gmail.com>
2023-12-19 12:59:56 +0300

    feat: add asin function tester component

--- src/components/testers/index.js
+++ src/components/testers/index.js
@@ -1,10 +1,12 @@
 import abs from '@/components/testers/functions/abs.vue'
 import acos from '@/components/testers/functions/acos.vue'
+import asin from '@/components/testers/functions/asin.vue'
 
 const testers = {
     functions: {
         'abs()': abs,
-        'acos()': acos
+        'acos()': acos,
+        'asin()': asin
     }
 }
 
