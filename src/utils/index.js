136a0f201623d46aeb0aff006b36efe8f1e8a016
bedirhandogan <bedirhandgn14@gmail.com>
2024-01-02 16:39:40 +0300

    refactor: get only numeric parts of versions

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,31 +3,41 @@ import bcd from '@mdn/browser-compat-data'
 export const getSupportList = (group, property) => {
     const support = bcd.css[group][property].__compat.support
 
+    const removeCharacter = (value) => {
+        const pattern = /\d+(\.\d+)?/
+
+        if (typeof value === 'string') {
+            return parseFloat(value.match(pattern))
+        }
+
+        return value
+    }
+
     return {
         chrome: {
-            version_desktop: support.chrome.version_added,
-            version_mobile: support.chrome_android.version_added
+            version_desktop: removeCharacter(support.chrome.version_added),
+            version_mobile: removeCharacter(support.chrome_android.version_added)
         },
         edge: {
-            version_desktop: support.edge.version_added
+            version_desktop: removeCharacter(support.edge.version_added)
         },
         firefox: {
-            version_desktop: support.firefox.version_added,
-            version_mobile: support.firefox_android.version_added
+            version_desktop: removeCharacter(support.firefox.version_added),
+            version_mobile: removeCharacter(support.firefox_android.version_added)
         },
         opera: {
-            version_desktop: support.opera.version_added,
-            version_mobile: support.opera_android.version_added
+            version_desktop: removeCharacter(support.opera.version_added),
+            version_mobile: removeCharacter(support.opera_android.version_added)
         },
         safari: {
-            version_desktop: support.safari.version_added,
-            version_mobile: support.safari_ios.version_added
+            version_desktop: removeCharacter(support.safari.version_added),
+            version_mobile: removeCharacter(support.safari_ios.version_added)
         },
         samsung_internet: {
-            version_mobile: support.samsunginternet_android.version_added
+            version_mobile: removeCharacter(support.samsunginternet_android.version_added)
         },
         webview: {
-            version_mobile: support.webview_android.version_added
+            version_mobile: removeCharacter(support.webview_android.version_added)
         }
     }
 }
