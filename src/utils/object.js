e98d0b07e0c18687e24029e472de694b4c0390b0
bedirhandogan <bedirhandgn14@gmail.com>
2024-01-16 17:30:40 +0300

    add: getCoordinateOnAxis function to utils

--- src/utils/object.js
+++ src/utils/object.js
@@ -5,3 +5,17 @@ export const flatObject = (object) => {
 export const getObjectLength = (object) => {
     return Object.keys(object).length
 }
+
+export const getCoordinateOnAxis = (event, axis) => {
+    if (event.touches === undefined) {
+        return {
+            x: event.clientX,
+            y: event.clientY
+        }[axis]
+    }
+
+    return {
+        x: event.touches[0].clientX,
+        y: event.touches[0].clientY
+    }[axis]
+}
